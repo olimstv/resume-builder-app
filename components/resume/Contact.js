61377f407602a636b0279c176ef1e4fdea4888b9
Oli Mostovyi <mostovoy.alexey@gmail.com>
2021-05-03 14:49:05 +0930

    resume: edit feature, pre-state

--- components/resume/Contact.js
+++ components/resume/Contact.js
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 const Contact = ({ basics }) => {
   const { email, phone, website, location, profiles } = basics;
 
--- pages/api/resumes/[id].js
+++ pages/api/resumes/[id].js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         }
         res.status(200).json({ success: true, data: resume });
       } catch (error) {
-        res.status(400).json({ success: false });
+        res.status(400).json({ msg: error.message });
       }
       break;
 
