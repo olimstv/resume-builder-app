559a30e0e16ec72a2384bbafd2e9637fca0396ea
Oli Mostovyi <mostovoy.alexey@gmail.com>
2021-05-05 10:45:10 +0930

    ProfileSelector: edit/add buttons (semantic-ui)

--- components/ProfileSelector.js
+++ components/ProfileSelector.js
@@ -8,7 +8,9 @@ import {
   List,
   ListItem,
   Segment,
-  Link
+  Link,
+  Button,
+  Reveal
 } from 'semantic-ui-react';
 export default function ProfileSelector(props) {
   const { profile } = props;
@@ -28,22 +30,42 @@ export default function ProfileSelector(props) {
         {' '}
         Profile
       </Header>
-      <div id='text-header'>
-        <Header as='h2'>
-          {profile.basics.name}
+
+      <Segment>
+        {/* <Button basic floated='right' icon='add'></Button> */}
+        <Button.Group floated='right'>
+          <Button icon='edit'></Button>
+          <Button.Or />
+          <Button positive icon='add'></Button>
+        </Button.Group>
+        <div>
+          <Header as='h2'>{profile.basics.name}</Header>
           <Label>
             <Icon name='code'></Icon> {profile.basics.label}
           </Label>
-        </Header>
-      </div>
+        </div>
+      </Segment>
+
       {/* ABOUT ME */}
       <Segment>
-        <Header as='h2' dividing>
-          <Icon name='user' size='small' />
-          About
-        </Header>
+        {/* <Button basic floated='right' icon='add'></Button>
+         */}
+        <Button.Group floated='right'>
+          <Button icon='edit'></Button>
+          <Button.Or />
+          <Button positive icon='add'></Button>
+        </Button.Group>
+        <div>
+          <Header as='h2' dividing>
+            <Icon name='user' size='small' />
+            About
+          </Header>
+        </div>
         <Segment>
-          <Label attached='top left'>Summary</Label>
+          <div>
+            <Label attached='top left'>Summary</Label>
+          </div>
+          <Button basic floated='right' icon='add'></Button>
           <div>{profile.basics.summary}</div>
         </Segment>
       </Segment>
@@ -53,6 +75,12 @@ export default function ProfileSelector(props) {
         <Label>Work Experience section doesn't filled yet...</Label>
       ) : (
         <Segment>
+          <Button.Group floated='right'>
+            <Button icon='edit'></Button>
+            <Button.Or />
+            <Button positive icon='add'></Button>
+          </Button.Group>
+
           <Header as='h2' dividing>
             <Icon name='suitcase' />
             Work Experience
@@ -61,6 +89,12 @@ export default function ProfileSelector(props) {
           {profile.work.map((exp, index) => {
             return (
               <Segment key={index}>
+                {/* <Button basic floated='right' icon='add'></Button> */}
+                <Button.Group floated='right'>
+                  <Button icon='edit'></Button>
+                  <Button.Or />
+                  <Button positive icon='add'></Button>
+                </Button.Group>
                 <Header as='h3'>{exp.company}</Header>
                 <Header.Subheader>
                   {exp.startDate} - {!exp.endDate ? `till now` : exp.endDate}
@@ -75,12 +109,19 @@ export default function ProfileSelector(props) {
                   <div>{exp.summary}</div>
                 </Segment>
                 <Segment>
+                  {/* <Button basic floated='right' icon='add'></Button> */}
                   <Header>Highlights</Header>
                   <List>
                     {exp.highlights &&
                       exp.highlights.map((item, index) => {
                         return (
                           <Segment>
+                            {/* <Button basic floated='right' icon='add'></Button> */}
+                            <Button.Group floated='right'>
+                              <Button icon='edit'></Button>
+                              <Button.Or />
+                              <Button positive icon='add'></Button>
+                            </Button.Group>
                             <ListItem key={index}>
                               <List.Icon name='check' />
                               <List.Content
@@ -106,6 +147,11 @@ export default function ProfileSelector(props) {
         <Label>Volunteer section doesn't filled yet...</Label>
       ) : (
         <Segment>
+          <Button.Group floated='right'>
+            <Button icon='edit'></Button>
+            <Button.Or />
+            <Button positive icon='add'></Button>
+          </Button.Group>
           <Header as='h2' dividing>
             <Icon name='users' />
             Volunteer
@@ -113,6 +159,11 @@ export default function ProfileSelector(props) {
           {profile.volunteer.map((exp, index) => {
             return (
               <Segment key={index}>
+                <Button.Group floated='right'>
+                  <Button icon='edit'></Button>
+                  <Button.Or />
+                  <Button positive icon='add'></Button>
+                </Button.Group>
                 <Header as='h3'>{exp.organization}</Header>
                 <Header.Subheader>
                   {exp.startDate} - {!exp.endDate ? `till now` : exp.endDate}
@@ -133,6 +184,11 @@ export default function ProfileSelector(props) {
                   {exp.highlights.map((item, index) => {
                     return (
                       <Segment>
+                        <Button.Group floated='right'>
+                          <Button icon='edit'></Button>
+                          <Button.Or />
+                          <Button positive icon='add'></Button>
+                        </Button.Group>
                         <ListItem key={index}>
                           <List.Icon name='check' />
                           <List.Content>{item}</List.Content>
@@ -149,19 +205,39 @@ export default function ProfileSelector(props) {
       {/* <!-- CONTACT --> */}
 
       <Segment>
+        <Button.Group floated='right'>
+          <Button icon='edit'></Button>
+          <Button.Or />
+          <Button positive icon='add'></Button>
+        </Button.Group>
         <Header dividing as='h2'>
           <Icon name='bullseye' /> Contact
         </Header>
-        <Segment>
+        <Segment padded>
+          <Button.Group floated='right'>
+            <Button icon='edit'></Button>
+            <Button.Or />
+            <Button positive icon='add'></Button>
+          </Button.Group>
           <Icon name='location arrow' />
           {profile.basics.location.city}, {profile.basics.location.region}{' '}
           {profile.basics.location.countryCode}
         </Segment>
-        <Segment>
+        <Segment padded>
+          <Button.Group floated='right'>
+            <Button icon='edit'></Button>
+            <Button.Or />
+            <Button positive icon='add'></Button>
+          </Button.Group>
           <Icon name='phone' />
           {profile.basics.phone}
         </Segment>
-        <Segment>
+        <Segment padded>
+          <Button.Group floated='right'>
+            <Button icon='edit'></Button>
+            <Button.Or />
+            <Button positive icon='add'></Button>
+          </Button.Group>
           <Icon name='mail' />
           {/* <a href={`mailto:${profile.basics.email}`}>{profile.basics.email}</a> */}
           <a href={`mailto:{profile.basics.email}`} target='_blank'>
@@ -169,7 +245,12 @@ export default function ProfileSelector(props) {
           </a>
         </Segment>
         {linkedIn.length > 0 && (
-          <Segment>
+          <Segment padded>
+            <Button.Group floated='right'>
+              <Button icon='edit'></Button>
+              <Button.Or />
+              <Button positive icon='add'></Button>
+            </Button.Group>
             <Icon name='linkedin' />
 
             <a href={linkedIn[0].url} target='_blank'>
@@ -179,7 +260,12 @@ export default function ProfileSelector(props) {
         )}
 
         {gitHub.length > 0 && (
-          <Segment>
+          <Segment padded>
+            <Button.Group floated='right'>
+              <Button icon='edit'></Button>
+              <Button.Or />
+              <Button positive icon='add'></Button>
+            </Button.Group>
             <Icon name='github' />
             <a href={gitHub[0].url} target='_blank'>
               {gitHub[0].username}
