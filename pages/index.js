22de483d1eca1813b7ceef82d4d8099628b07fcd
Anton Andriievskyi <anton.andriievskyi@toptal.com>
2021-05-08 00:34:49 +0930

    Simple Home page

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,56 @@
 import Layout from "../components/Layout";
-import {Header} from "semantic-ui-react";
+import {Header, Step, Icon} from "semantic-ui-react";
 import withSession, {extractReqResFromArgs, useUserServerSide} from "../util/session";
 
 export default function Home({user}) {
     return <Layout user={user}>
-        <Header as='h2'>Let's Get IT</Header>
+        <Header as='h2'>Welcome to lets.get(IT)!</Header>
 
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            commodi, consequuntur distinctio, laboriosam quia, voluptates vitae
-            facere repudiandae id autem eligendi quod vero nesciunt dolores quaerat
-            enim! Suscipit, nesciunt expedita.
+            Compose multiple versions of your CV, each tuned individually for a particular role.
         </p>
+
+        <Step.Group vertical>
+            <Step>
+                <Icon name='signup' />
+                <Step.Content>
+                    <Step.Title>Profile</Step.Title>
+                    <Step.Description>
+                        Create a detailed version of your Profile.
+                    </Step.Description>
+                </Step.Content>
+            </Step>
+            <Step>
+                <Icon name='list' />
+                <Step.Content>
+                    <Step.Title>CVs</Step.Title>
+                    <Step.Description>
+                        Create unlimited individual CVs in a WYSIWYG editor.
+                    </Step.Description>
+                </Step.Content>
+            </Step>
+            <Step>
+                <Icon name='linkify' />
+                <Step.Content>
+                    <Step.Title>Public Links</Step.Title>
+                    <Step.Description>
+                        Assign a public link for each CV and share with employers.
+                    </Step.Description>
+                </Step.Content>
+            </Step>
+            <Step>
+                <Icon name='file pdf' />
+                <Step.Content>
+                    <Step.Title>Download PDF</Step.Title>
+                    <Step.Description>
+                        Both you and employer can download the CV as a PDF.
+                    </Step.Description>
+                </Step.Content>
+            </Step>
+        </Step.Group>
+
         <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            commodi, consequuntur distinctio, laboriosam quia, voluptates vitae
-            facere repudiandae id autem eligendi quod vero nesciunt dolores quaerat
-            enim! Suscipit, nesciunt expedita.
+            We store your profile in Open Source <a href='https://jsonresume.org/' target='json_resume_open_source'>JSON Resume</a> format.
         </p>
     </Layout>
 }
